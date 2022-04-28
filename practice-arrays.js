07440a157a78e9fab7e992af4cd48d1bacb72b81
AnarMikailov <a.v.mikayilov@gmail.com>
2022-04-28 12:14:16 +0400

    initial commit

--- practice-arrays.js
+++ practice-arrays.js
@@ -1,1 +1,19 @@
-console.log('salam');
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+//Filter method with arrow function
+const deposit = movements.filter(mov => mov > 0);
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(deposit);
+// const depositFor = [];
+// for (const mov of movements) if (mov > 0) depositFor.push(mov);
+// console.log(depositFor);
+
+//Reduce method
+
+/////////////////////////////////////////////////
