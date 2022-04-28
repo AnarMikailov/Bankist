5896e1544a08e35e6456c5a1384ef858419d1636
AnarMikailov <a.v.mikayilov@gmail.com>
2022-04-28 12:15:54 +0400

    initial commit

--- script.js
+++ script.js
@@ -74,24 +74,3 @@ const creatUserName = function (accs) {
   });
 };
 creatUserName(accounts);
-
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
-
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-const deposit = movements.filter(mov => mov > 0);
-const withdrawals = movements.filter(mov => mov < 0);
-console.log(deposit);
-// const depositFor = [];
-// for (const mov of movements) if (mov > 0) depositFor.push(mov);
-// console.log(depositFor);
-
-/////////////////////////////////////////////////
